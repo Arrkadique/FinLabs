af51dededab8feff1a22e5e6aba1de49fcd8c098
Arkady Sokol <arrkadique@gmail.com>
2023-11-30 16:29:28 +0300

    initial commit

---
+++ src/test/java/com/arrkadique/bankingsystem/BankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arrkadique.bankingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
